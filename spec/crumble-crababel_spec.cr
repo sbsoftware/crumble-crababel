19b8f830a8aa5276b7e1541323d53aaaf993de23
Stefan Bilharz <stefan@sbsoftware.de>
2026-02-05 19:40:00 +0100

    CRCR-1 Prevent crash on missing Accept-Language header

--- spec/crumble-crababel_spec.cr
+++ spec/crumble-crababel_spec.cr
@@ -1,9 +1,39 @@
 require "./spec_helper"
+require "http"
+
+private struct FakeCtx
+  getter request
+
+  def initialize(@request : HTTP::Request)
+  end
+end
+
+private class Greeting
+  include Crumble::Crababel
+
+  getter ctx
+
+  def initialize(@ctx : FakeCtx)
+  end
+
+  def message
+    t
+  end
+end
 
 describe Crumble::Crababel do
-  # TODO: Write tests
+  it "falls back to the default locale when Accept-Language is missing" do
+    request = HTTP::Request.new("GET", "/", headers: HTTP::Headers.new)
+    greeting = Greeting.new(FakeCtx.new(request))
 
-  it "works" do
-    false.should eq(true)
+    greeting.message.should eq("Hello")
+  end
+
+  it "uses the Accept-Language header when present" do
+    headers = HTTP::Headers{"Accept-Language" => "de"}
+    request = HTTP::Request.new("GET", "/", headers: headers)
+    greeting = Greeting.new(FakeCtx.new(request))
+
+    greeting.message.should eq("Hallo")
   end
 end
--- src/ext/crumble/crababel.cr
+++ src/ext/crumble/crababel.cr
@@ -3,7 +3,16 @@ require "http_accept"
 module Crumble
   module Crababel
     macro t
-      locale = HTTP::Accept::Language.best_locale(::Crababel.locales, HTTP::Accept::Language.parse(ctx.request.headers["Accept-Language"]), "en")
+      accept_language = ctx.request.headers["Accept-Language"]?
+      locale = if accept_language
+        HTTP::Accept::Language.best_locale(
+          ::Crababel.locales,
+          HTTP::Accept::Language.parse(accept_language),
+          "en",
+        )
+      else
+        "en"
+      end
       ::Crababel.locale(locale).{{ @type.name.split("::").map(&.underscore).join(".").id }}
     end
   end
